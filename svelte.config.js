a69b8c194b52f0619cb7b8106047f9e2971d33dd
Chris Nanney <cnanney@gmail.com>
2023-01-26 21:19:03 -0600

    Prep example

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : '/sv-preload-code',
+		},
+		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+		// For example, instead of '_app', use 'app_', 'internal', etc.
+		appDir: 'internal',
 	}
 };
 
